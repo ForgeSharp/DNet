5d1ea219c98703d666db6f3168bfe3c604188614
CloudRex <cloudrex@outlook.com>
2018-11-29 15:29:00 -0500

    Add .paket directory

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
-﻿@echo off
-echo "Building DNET ..."
-dotnet restore
+﻿@echo Building DNET
+@echo Remove old directories
+rmdir packages /s /q
+rmdir paket-files /s /q
+@echo Run paket install
+paket install
+@echo Run dotnet build
+dotnet build
