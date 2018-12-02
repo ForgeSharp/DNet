cd1c224d729344ec3e4990d10e6c168c8f1f534e
CloudRex <cloudrex@outlook.com>
2018-12-02 02:10:15 -0500

    Add reminder

--- Builders/Builder.cs
+++ Builders/Builder.cs
@@ -1,5 +1,6 @@
 ﻿namespace DNet.Builders
 {
+    // TODO: Builders should be able to be casted to generic type provided
     public interface IBuilder<BuiltObjectType>
     {
         BuiltObjectType Build();
